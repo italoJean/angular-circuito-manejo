b7959f3bf1c3b52d810097e3c56a7f9bdaf6be1f
italoJean <italocarlos032@gmail.com>
2025-12-21 11:37:24 -0500

    update circuito-manejo v.2.1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: false,
-    API_URL : 'http://localhost:8080/api/v1'
+    
+    production: true,
+    API_URL : 'https://spring-boot-rest-circuito-manejo-production.up.railway.app/api/v1'
+    // production: false,
+    // API_URL : 'http://localhost:8080/api/v1'
 };
