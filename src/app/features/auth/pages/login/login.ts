b7b085f2790c8aeccd5b9c3221e5eb519f3c5c7f
italoJean <italocarlos032@gmail.com>
2025-12-28 14:41:36 -0500

    deploy v.2.1.1

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class Login {
 loginConGoogle() {
   // Redirigimos al endpoint de Spring Boot que inicia el flujo de OAuth2
-  window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  // window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  window.location.href = 'https://spring-boot-rest-circuito-manejo-production.up.railway.app/oauth2/authorization/google';
 }
 }
